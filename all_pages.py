7d0ee9c790035b50a289cec6db90a29fd308b26a
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2023-04-28 13:51:36 -0400

    Add files via upload

---
+++ all_pages.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import datetime
+
+file = open('all_pages.txt', 'r')
+#contents = []
+#with open('bike_list2.csv', 'w+', newline='', errors="ignore") as i:
+#    write = csv.DictWriter(i, fieldnames=['Title', 'Price', 'Views', 'Watches', 'Views/Watches', 'Link'])
+#    write.writeheader()
+
+for line in file.readlines():
+    response = requests.get(line)
+    html = response.content
+    soup = BeautifulSoup(html, 'html.parser')
+    try:
+        title = soup.find('h1').get_text()
+        print(title)
+        good_soup = soup.find_all('div', {'class': 'buysell-details-column'})[1].contents
+        views = int(good_soup[-5].strip().replace(",",""))
+        watches = int(good_soup[-1].strip())
+        try:
+            price = soup.find('div', {'class': 'buysell-container buysell-price'}).get_text().strip()
+
+            price = price.replace(',','').replace('$','').replace('USD','')
+            price = int(price)
+        except ValueError:
+            continue
+        print(price)
+        datetime_object = datetime.datetime.strptime(good_soup[2].strip(), '%b-%d-%Y %H:%M:%S')
+        diff = datetime.datetime.now()-datetime_object
+        with open('all_pages.csv', 'a+', newline='', errors="ignore") as csv_file:
+            write = csv.DictWriter(csv_file, fieldnames=['Title', 'Price', 'Views', 'Watches', 'Views/Watches', 'Link'])
+            write.writerow({'Title': title, 'Price': price, 'Views': views, 'Watches': watches, 'Link': line.strip()})
+    except AttributeError:
+        continue
+
+
+file.close()
